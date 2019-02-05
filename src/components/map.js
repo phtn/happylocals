983d80b5a2cf308cd7d914ec494690a4230cc584
phtn458 <phtn458@gmail.com>
2019-02-05 01:34:54 -0500

    added google maps

--- src/components/map.js
+++ src/components/map.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import { Map, GoogleApiWrapper} from 'google-maps-react'
 
 const styles = {
   container: {
     height: 400
   }
 }
-function Map() {
+function MapContainer(props) {
+  const { google } = props
   return (
-    <div style={styles.container}>
-      Map
+    <div id='map' style={styles.container}>
+    {/*  29.839496642 -81.26999892 */}
+      <Map google={google} zoom={14} initialCenter={{lat: 29.839496642, lng: '-81.26999892'}}/>
     </div>
   )
 }
 
-export default Map
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAmP8DMo4zq8HXROwfyxmEVjvmdKY3rWsE"
+})(MapContainer)
